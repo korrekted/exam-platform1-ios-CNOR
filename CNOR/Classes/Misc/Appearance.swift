d476464ef4d39f5ccfcea96e6cfd9bc8f2dd9ffd
akonst17 <akonst17@gmail.com>
2022-04-15 11:59:19 +0300

    upgrade; build 3

--- CNOR/Classes/Misc/Appearance.swift
+++ CNOR/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 136, green: 192, blue: 207)
     static let progress2Color = UIColor(integralRed: 219, green: 84, blue: 73)
     static let progress3Color = UIColor(integralRed: 69, green: 105, blue: 145)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- CNOR/Classes/Service/Stats/Share/StatsShareManager.swift
+++ CNOR/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.cnor.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.cnor.widget")
     }
     
     @discardableResult
