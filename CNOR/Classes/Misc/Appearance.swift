de5530b2e1f6cbc94aa978f56a7973dad2df875f
akonst17 <akonst17@gmail.com>
2022-08-09 11:21:06 +0300

    обновления Nursing; build 4

--- CNOR/Classes/Misc/Appearance.swift
+++ CNOR/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 255)
-    
     static let mainColor = UIColor(integralRed: 136, green: 192, blue: 207)
     static let mainColorAlpha = UIColor(integralRed: 136, green: 192, blue: 207, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 219, green: 84, blue: 73)
-    
-    static let progress1Color = UIColor(integralRed: 136, green: 192, blue: 207)
-    static let progress2Color = UIColor(integralRed: 219, green: 84, blue: 73)
-    static let progress3Color = UIColor(integralRed: 69, green: 105, blue: 145)
-    
+    static let secondColor = UIColor(integralRed: 69, green: 105, blue: 145)
+    static let thirdColor = UIColor(integralRed: 219, green: 84, blue: 73)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 255)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
