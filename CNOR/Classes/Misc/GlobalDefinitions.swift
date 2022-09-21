ffcd2819c48e4c897bc2349f391beb393045c1d7
akonst17 <akonst17@gmail.com>
2022-09-21 12:10:03 +0300

    интеграция sdk 2.0; новые урлы; build 7

--- CNOR/Classes/Misc/GlobalDefinitions.swift
+++ CNOR/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "jncYrDGnULOTVYhL"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "ZTVmvdumy9Esqntv"
 
     static let applicationTag = "EXAM_CNOR"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "ZTVmvdumy9Esqntv"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "pVmPbYGsy1lilB42"
 
     static let appStoreUrl = "https://apps.apple.com/app/cnor-exam-test-prep/id1584529078"
     static let appleAppID = "1584529078"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
